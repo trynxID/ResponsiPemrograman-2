faf17ad56fc765ab646c053ccd5e28b23b918f3e
trynxID <sidiq.firman48@gmail.com>
2022-07-15 10:19:13 +0700

    project mahasiswa

---
+++ ProjectMahasiswa/Mahasiswa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectMahasiswa
+{
+    public class Mahasiswa
+    {
+        public string Nim { get; set; }
+        public string Nama { get; set; }
+        public string JenisKelamin { get; set; }
+        public string IPK { get; set; }
+    }
+}
